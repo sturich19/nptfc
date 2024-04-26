dbd8063424fbf03315e2621394bac5fcd43c8e44
sturich19 <61119964+sturich19@users.noreply.github.com>
2024-04-26 15:40:19 +0100

    Added U8 fixtures and results to the database

--- src/utils/season-utils.tsx
+++ src/utils/season-utils.tsx
@@ -1,11 +1,25 @@
 import { Season } from "../objects/season";
 import U9Div2Image from '../images/u9-div2-24-min.jpg'
 import U9Div3Image from '../images/u9-div3-23-min.jpg'
+import U8Div2Image from '../images/u8-div2-22-min.jpg'
+import U8Div3Image from '../images/u8-div3-23-min.jpg'
 
 export function GetImageToUse (season : Season) {
 
     switch (season.ageGroup)
     {
+        case 8:
+        {
+                switch (season.startYear)
+                {
+                    case 2022:
+                        return U8Div2Image;
+                    
+                    case 2023:
+                        return U8Div3Image;
+                }    
+            return U8Div2Image
+        }
         case 9:
         {
             switch (season.startYear)
